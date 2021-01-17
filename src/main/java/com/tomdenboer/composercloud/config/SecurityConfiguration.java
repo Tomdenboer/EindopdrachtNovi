ae8c99a9f77bb8983067959cf8bcf29d9b8adb6e
Tom den Boer <tomdenboer@live.nl>
2021-01-17 20:55:53 +0100

    bugfixes, bcrypt works now

--- src/main/java/com/tomdenboer/composercloud/config/SecurityConfiguration.java
+++ src/main/java/com/tomdenboer/composercloud/config/SecurityConfiguration.java
@@ -25,14 +25,15 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-      auth.userDetailsService(userDetailsService);
+      auth.userDetailsService(userDetailsService)
+      .passwordEncoder(getPasswordEncoder());
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.authorizeRequests()
-                .antMatchers("/").permitAll()
+                .antMatchers("/**").hasRole("USER")
                 .and()
                 .httpBasic();
     }
